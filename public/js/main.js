69aacd2b156739723562fe205749a8ef608a774b
iTrollyou <pguerrero002@ikasle.ehu.eus>
2020-04-01 14:09:11 +0200

    Buster v2

--- public/js/main.js
+++ public/js/main.js
@@ -1,18 +1,12 @@
-function loadImage(url) {
-    return new Promise(resolve => {
-        const image = new Image();
-        image.addEventListener('load', () => {
-            resolve(image);
-        });
-        image.src = url;
-    })
-}
+import {loadImage} from './loaders.js';
+import SpriteSheet from "./SpriteSheet.js";
+
 
 const canvas = document.getElementById('screen');
 const context = canvas.getContext('2d');
 
-
-loadImage('img/sprites.png')
-    .then(image => {
-        context.drawImage(image, 0, 0)
-    });
+loadImage('img/sprites.png').then(image => {
+    const sprites = new SpriteSheet(image, 32, 32);
+    sprites.define('buster', 0, 0);
+    sprites.draw('buster', context, 150, 150);
+});

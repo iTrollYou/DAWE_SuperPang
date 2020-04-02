4283c7ae6bd0535c22dba60e859b594b2b5a1fcc
iTrollyou <pguerrero002@ikasle.ehu.eus>
2020-04-02 09:02:12 +0200

    Niveles y JSON

--- public/js/loaders.js
+++ public/js/loaders.js
@@ -3,6 +3,13 @@ import Player from "./Player.js";
 import {Vec2D} from "./Vec2D.js";
 import Settings from "./Settings.js";
 
+
+export function loadLevel(currentLevel) {
+    return fetch(`levels/${currentLevel}.json`)
+        .then(r => r.json());
+
+}
+
 export function loadImage(url) {
 
     return new Promise(resolve => {
@@ -15,13 +22,13 @@ export function loadImage(url) {
 }
 
 
-export function loadBuster(image) {
+export function loadBuster(image,playerSpec) {
     const spriteSheet = new SpriteSheet(image, 32, 32);
     spriteSheet.define('buster', 1, 0);
     spriteSheet.define('buster-1', 2, 0);
     spriteSheet.define('buster-2', 3, 0);
     spriteSheet.define('buster-3', 4, 0);
-    const pos = new Vec2D(Settings.SCREEN_WIDTH / 2 - 16, Settings.SCREEN_HEIGHT - 32);
+    const pos = new Vec2D(playerSpec.pos[0], playerSpec.pos[1]);
     const size = new Vec2D(32, 32);
 
     return new Player(size, pos, spriteSheet);
--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,4 @@
-import {loadImage, loadBuster} from './loaders.js';
+import {loadImage, loadBuster, loadLevel} from './loaders.js';
 import Settings from "./Settings.js";
 import {setupKeyboard} from "./input.js";
 
@@ -11,27 +11,27 @@ Settings.SCREEN_HEIGHT = canvas.height;
 Settings.SCREEN_WIDTH = canvas.width;
 
 
-loadImage('img/sprites.png').then(image => {
+Promise.all([loadImage('img/sprites.png'), loadLevel('1')])
+    .then(([image, levelSpec]) => {
+        const buster = loadBuster(image, levelSpec.player);
+        let deltaTime = 0;
+        let lastTime = 0;
 
-    const buster = loadBuster(image);
+        function update(time) {
+            deltaTime = time - lastTime;
+            context.clearRect(0, 0, canvas.width, canvas.height);
+            // pintar contorno
+            context.strokeRect(0, 0, canvas.width, canvas.height);
+            buster.draw(context);
+            buster.update(deltaTime / 1000);
+            lastTime = time;
+            requestAnimationFrame(update);
+        }
 
-    let deltaTime = 0;
-    let lastTime = 0;
+        const input = setupKeyboard(buster);
+        input.listenTo(window);
 
-    function update(time) {
-        deltaTime = time - lastTime;
-        context.clearRect(0, 0, canvas.width, canvas.height);
-        // pintar contorno
-        context.strokeRect(0, 0, canvas.width, canvas.height);
-        buster.draw(context);
-        buster.update(deltaTime / 1000);
-        lastTime = time;
-        requestAnimationFrame(update);
-    }
+        update(0);
+    });
 
-    const input = setupKeyboard(buster);
-    input.listenTo(window);
 
-    update(0);
-
-});
